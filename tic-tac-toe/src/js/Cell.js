72aba6e5750fd02ea76c2f0f7df0256734b51d12
Курцева Н.О <kurzevano@gmail.com>
2021-07-09 15:39:19 +0700

    - Удалено неиспользуемое свойство

--- tic-tac-toe/src/js/Cell.js
+++ tic-tac-toe/src/js/Cell.js
@@ -1,24 +1,9 @@
 export default class Cell {
     constructor() {
         this.value = null;
-
-        // weight используется для определения выигрыша
-        this.weight = 0;
     }
 
     setValue(val) {
         this.value = val;
-
-        // weight используется для определения выигрыша
-        if (val == "X") {
-            console.log(1);
-            this.weight = 1;
-        } else if (this.value == "O") {
-            console.log(-1);
-            this.weight = -1;
-        } else {
-            console.log(0);
-            this.weight = 0;
-        }
     }
 }
